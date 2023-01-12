a48e26a2341f3d163f9039e481a53827999f852f
Lia Ximenes <ximeneslia@gmail.com>
2023-01-12 00:35:18 -0300

    exercicio completo

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int number = 5;
+
+        System.out.println("Tabela de multiplicação de " + number);
+
+        for(int i = 1; i <= 10; i++){
+            System.out.println(i + "x" + number + "=" + (i*number) );
+        }
+
+    }
+}
